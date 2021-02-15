185d40d37ceaf473a4f25bc015be7b432d84c02e
Alejandro Ramirez <627538+janodev@users.noreply.github.com>
2021-02-15 17:08:38 +0100

    Update Package.swift
    update Package.swift with new github user

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
         .library(name: "Collection", type: .static, targets: ["Collection"])
     ],
     dependencies: [
-        .package(url: "git@github.com:j4n0/autolayout.git", from: "1.0.0"),
-        .package(url: "git@github.com:j4n0/log.git", from: "1.0.0"),
+        .package(url: "git@github.com:janodev/autolayout.git", from: "1.0.0"),
+        .package(url: "git@github.com:janodev/log.git", from: "1.0.0"),
         .package(url: "git@github.com:apple/swift-log.git", from: "1.2.0")
     ],
     targets: [
